913a37c1144f552a35eecede771f9e8d2ff5a467
Ruben del Blanco Rey <rubendelblanco@gmail.com>
2017-06-25 19:26:08 +0200

    validacion terminada

--- f4p_form_reservas.php
+++ f4p_form_reservas.php
@@ -20,19 +20,42 @@
   }
  ?>
 
-<form action="<?php echo $f4p_form_action; ?>" method="POST">
-  Fecha entrada* <input type="text" placeholder="Entrada"  id="calendar-entrada" name="entrada"/> <span data-target="#calendar-entrada" class="calendar-trigger dashicons dashicons-calendar-alt"></span>
-  Fecha salida* <input type="text" placeholder="Salida" id="calendar-salida" name="salida"/><span data-target="#calendar-salida" class="calendar-trigger dashicons dashicons-calendar-alt"></span>
+<form id="formReserva" action="<?php echo $f4p_form_action; ?>" method="POST">
+  Fecha entrada* <input class="required" type="text" placeholder="Entrada"  id="calendar-entrada" readonly name="entrada" required data-msg-required="Campo 'Fecha entrada' obligatorio"/>
+  <span data-target="#calendar-entrada" class="calendar-trigger dashicons dashicons-calendar-alt"></span>
+  Fecha salida* <input clas="required" type="text" placeholder="Salida" id="calendar-salida" name="salida" readonly required data-msg-required="Campo 'Fecha salida' obligatorio"/>
+  <span data-target="#calendar-salida" class="calendar-trigger dashicons dashicons-calendar-alt"></span>
   Código promoción <input type="text" name="codpromo" placeholder="Código promocional" />
+  <div class="mensajes-error"></div>
   <input type="submit" value="Consultar disponibilidad">
 </form>
-
+<style>
+  .mensajes-error label{
+    color:red;
+  }
+</style>
 <script>
   jQuery('.calendar-trigger').click(function(){
     jQuery(jQuery(this).data('target')).trigger('focus');
   });
   jQuery( function() {
+    jQuery( "#calendar-entrada, #calendar-salida" ).datepicker({
+      dateFormat: "dd/mm/yy"
+    });
     jQuery( "#calendar-entrada" ).datepicker();
     jQuery( "#calendar-salida" ).datepicker();
   } );
+  jQuery("#formReserva").validate({
+    errorLabelContainer: jQuery(".mensajes-error"),
+    rules: {
+        'calendar-entrada':{
+          required: true,
+          date:true
+        },
+        'calendar-salida':{
+          required: true,
+          date:true
+        }
+  		}
+  });
   </script>
--- form4postes.php
+++ form4postes.php
@@ -14,10 +14,12 @@ function f4p_desplegar_formulario(){
 }
 
 wp_register_script('jqueryUI', plugins_url('js/jquery-ui/jquery-ui.min.js', __FILE__), array('jquery'),null, true);
+wp_register_script('jqueryValidation', plugins_url('js/jquery-validation/dist/jquery.validate.min.js', __FILE__), array('jquery'),null,false);
 wp_register_style( 'f4pstructure', plugins_url( 'js/jquery-ui/jquery-ui.min.css', __FILE__) );
 wp_register_style( 'f4ptheme', plugins_url( 'js/jquery-ui/jquery-ui.theme.min.css', __FILE__) );
 wp_enqueue_style('f4pstructure');
 wp_enqueue_script('jqueryUI');
 wp_enqueue_style('f4ptheme');
+wp_enqueue_script('jqueryValidation');
 add_shortcode( 'form4postes', 'f4p_desplegar_formulario' );
  ?>
